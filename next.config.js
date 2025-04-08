5210d064398e4301a43946ac95fcae2173341957
harshcustomaistudio <harsh@customaistudio.io>
2025-04-08 18:50:59 +0530

    harsh soni

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   images: {
     unoptimized: true
   },
   // Simplify configuration
-  distDir: '.next'
 };
 
 module.exports = nextConfig; 
